2f5353cb0b3254608b287ec755fe90c6f8a757df
Dan <danretegan@yahoo.com>
2024-07-11 12:31:26 +0300

    Update calorieInfoRoutes.js

--- routes/api/calorieInfoRoutes.js
+++ routes/api/calorieInfoRoutes.js
@@ -34,7 +34,20 @@ router.post("/save-calorie-info", validateAuth, async (req, res) => {
     await user.save();
     res.status(200).json({ message: "Calorie info saved successfully" });
   } catch (error) {
-    console.log(error); // Adaugă acest log pentru debugging
+    console.log("Error saving calorie info", { error });
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/get-calorie-info", validateAuth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user || !user.calorieInfo) {
+      return res.status(404).json({ message: "Calorie info not found" });
+    }
+    res.status(200).json(user.calorieInfo);
+  } catch (error) {
+    console.log("Error fetching calorie info", { error });
     res.status(500).json({ message: error.message });
   }
 });
